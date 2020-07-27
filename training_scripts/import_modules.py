0fad9a17936e2e00b239b60237a3f51248b0b326
Satyarth Praveen <satyarth934@gmail.com>
2020-07-26 21:26:17 -0400

    Converting notebook to scripts

---
+++ training_scripts/import_modules.py
@@ -0,0 +1,33 @@
+import os
+from os import path
+import sys
+import math
+import cv2  # Read raw image
+import glob
+import random
+import numpy as np
+import pickle
+from matplotlib import pyplot as plt
+from scipy import ndimage  # For rotation task or
+import imutils
+from skimage.color import rgb2lab, lab2rgb, rgb2gray
+from skimage.io import imsave
+from skimage.transform import resize
+from pprint import pprint
+
+import tensorflow
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Dense, Flatten, Dropout
+from tensorflow.keras.optimizers import Adam
+from tensorflow.python.keras.utils import data_utils
+from tensorflow.keras.preprocessing.image import Iterator
+from tensorflow.keras import Input
+from tensorflow.keras.layers import Conv2D, Conv2DTranspose, UpSampling2D, AveragePooling2D, MaxPooling2D, Reshape, Conv2DTranspose, ZeroPadding2D, Add
+from tensorflow.keras.layers import Activation, InputLayer, BatchNormalization
+from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from tensorflow.keras.layers import PReLU
